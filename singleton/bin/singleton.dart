53f760ad70f9db6ec9c8b00be76f21e09981838b
“mibcoder” <mibcoder@gmail.com>
2022-08-09 21:00:05 +0500

    singleton sample implementation both Early and Lazy Instantiation

--- singleton/bin/singleton.dart
+++ singleton/bin/singleton.dart
@@ -1,5 +1,43 @@
-import 'package:singleton/singleton.dart' as singleton;
+main() {
 
-void main(List<String> arguments) {
-  print('Hello world: ${singleton.calculate()}!');
+  //To use Early Instantiation
+  EarlyInstantiationSingleton earlyInstantiationSingleton =
+      EarlyInstantiationSingleton();
+  earlyInstantiationSingleton.doSomething();
+
+  //To use Lazy Instantiation
+  LazyInstantiationSingleton lazyInstantiationSingleton =
+      LazyInstantiationSingleton.instance;
+  lazyInstantiationSingleton.doSomething();
+}
+
+//Early Instantiation
+class EarlyInstantiationSingleton {
+  EarlyInstantiationSingleton._privateConstructor();
+
+  static final EarlyInstantiationSingleton _instance =
+      EarlyInstantiationSingleton._privateConstructor();
+
+  factory EarlyInstantiationSingleton() {
+    return _instance;
+  }
+
+  void doSomething() {
+    print('doSomething in Early Instantiation Singleton');
+  }
+}
+
+//Lazy Instantiation
+class LazyInstantiationSingleton {
+  LazyInstantiationSingleton._privateConstructor();
+
+  static LazyInstantiationSingleton? _instance;
+
+  static LazyInstantiationSingleton get instance {
+    return _instance ??= LazyInstantiationSingleton._privateConstructor();
+  }
+
+  void doSomething() {
+    print('doSomething in Lazy Instantiation Singleton');
+  }
 }
