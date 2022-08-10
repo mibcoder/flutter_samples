de0cb3d1aac1d80f3b497c381dcadef09a700ebf
“mibcoder” <mibcoder@gmail.com>
2022-08-10 06:36:58 +0500

    add comment for simple 3 steps to create singleton

--- singleton/bin/singleton.dart
+++ singleton/bin/singleton.dart
@@ -13,11 +13,15 @@ main() {
 
 //Early Instantiation
 class EarlyInstantiationSingleton {
+
+  //Step#1 Create private constructor
   EarlyInstantiationSingleton._privateConstructor();
 
+  //Step#2 create private static variable to store instance of this class
   static final EarlyInstantiationSingleton _instance =
       EarlyInstantiationSingleton._privateConstructor();
 
+  //Step#3 return instance of this class through factory constructor
   factory EarlyInstantiationSingleton() {
     return _instance;
   }
@@ -29,10 +33,14 @@ class EarlyInstantiationSingleton {
 
 //Lazy Instantiation
 class LazyInstantiationSingleton {
+
+  //Step#1 Create private constructor
   LazyInstantiationSingleton._privateConstructor();
 
+  //Step#2 create private static variable to store instance of this class
   static LazyInstantiationSingleton? _instance;
 
+  //Step#3 return instance of this class through getter
   static LazyInstantiationSingleton get instance {
     return _instance ??= LazyInstantiationSingleton._privateConstructor();
   }
